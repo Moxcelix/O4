80a500e9de1ce47d528e717f640df3a264154bc1
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-05-29 15:31:56 +0300

    finaly

--- Assets/Scripts/Main API/CharacterRepresenter.cs
+++ Assets/Scripts/Main API/CharacterRepresenter.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CharacterRepresenter : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform _testCube;
 
     private GameObject _characterInstance = null;
 
@@ -23,8 +24,18 @@ public class CharacterRepresenter : MonoBehaviour
         var data = Level.Instance.GetCurrentCharacter();
 
         if (data == null)
+        {
             return;
+        }
 
+        if(data.character == null)
+        {
+            _testCube.gameObject.SetActive(true);
+
+            return;
+        }
+
+        _testCube.gameObject.SetActive(false);
         InstantiateCharacter(data.character);
     }
 
